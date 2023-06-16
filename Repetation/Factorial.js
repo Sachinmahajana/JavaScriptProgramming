2c9d0a042b4895526c901a1ed695ce65d9cee2d3
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-16 22:32:23 +0530

    [Sachin] Adding Repetition

---
+++ Repetation/Factorial.js
@@ -0,0 +1,9 @@
+//Q Write a program that computes a
+// factorial of a number taken as a input
+
+let num = 5
+let fact = 1;
+for(let i = 1; i<=num; i++){
+     fact = fact * i;
+}
+console.log(fact);
